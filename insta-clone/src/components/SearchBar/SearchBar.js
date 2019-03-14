065d73acf35432171ab716d7d458b4e08af3bc8d
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-13 19:43:03 -0700

    styled SearchBar

--- insta-clone/src/components/SearchBar/SearchBar.js
+++ insta-clone/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { FaInstagram } from "react-icons/fa";
+import { TiCompass } from "react-icons/ti";
+import { FiHeart } from "react-icons/fi";
+import { MdPersonOutline } from "react-icons/md";
+
+// Styles
+import "./SearchBar.scss";
 
 const SearchBar = ({ onInputChange }) => {
-
-        return (
-            <input
-                type={"text"}
-                placeholder={"Search"}
-                onChange={event => onInputChange(event)}
-            />
-        )
-
-}
+  return (
+    <div className={"header"}>
+      <div className={"leftSide"}>
+        <FaInstagram className={"icon"} size={"3em"} />
+        <h1>Instaclone</h1>
+      </div>
+      <input
+        type={"text"}
+        placeholder={"\u2315 Search"}
+        onChange={event => onInputChange(event)}
+      />
+      <div className={"interactions"}>
+        <TiCompass className={"interaction"} size={"2em"} color={"dimgrey"} />
+        <FiHeart className={"interaction"} size={"1.8em"} color={"dimgrey"} />
+        <MdPersonOutline
+          className={"interaction"}
+          size={"2em"}
+          color={"dimgrey"}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
