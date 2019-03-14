aaa8bea170e296f7ab2cb5aa9888efb105dd50a1
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-13 20:26:43 -0700

    styled PostFooter

--- insta-clone/src/components/PostContainer/PostFooter.js
+++ insta-clone/src/components/PostContainer/PostFooter.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { FiHeart } from "react-icons/fi";
+import { FaRegComment } from "react-icons/fa";
 
 import CommentSection from "../CommentSection/CommentSection";
 
-const PostFooter = props => {
-  return <CommentSection />;
+const PostFooter = ({ likes, comments, timestamp }) => {
+  return (
+    <div className={"postFooter"}>
+      <div className={"interactions"}>
+        <FiHeart className={"interaction"} size={"1.6em"} />
+        <FaRegComment className={"interaction"} size={"1.6em"} />
+      </div>
+      <h2>{likes} likes</h2>
+      <CommentSection comments={comments} />
+    </div>
+  );
 };
 
 export default PostFooter;
