5711fb0acb5873667d30f4df61ccb2c043810bf1
Rane Wallin <RAN2143066@maricopa.edu>
2019-03-19 17:55:23 -0700

    scaffold login

--- insta-clone/src/components/LoginPage/Login.js
+++ insta-clone/src/components/LoginPage/Login.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+const Login = ({ loginClicked }) => {
+  return (
+    <div className="login">
+      <form onSubmit={loginClicked}>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
